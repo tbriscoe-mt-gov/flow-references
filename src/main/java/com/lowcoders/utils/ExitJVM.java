084702a33a510466014bcbe4652dc01f5d525dd3
Tom Briscoe <tbriscoe@mt.gov>
2017-01-23 20:58:37 -0700

    Update ExitJVM.java

--- src/main/java/com/lowcoders/utils/ExitJVM.java
+++ src/main/java/com/lowcoders/utils/ExitJVM.java
@@ -1,15 +1,18 @@
 package com.lowcoders.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.mule.api.MuleEventContext;
 import org.mule.api.lifecycle.Callable;
 
 
 public class ExitJVM implements Callable {
 
+	Logger log = LoggerFactory.getLogger(ExitJVM.class);
 
 	@Override
 	public Object onCall(MuleEventContext eventContext) throws Exception {
-
-		    System.exit(0);
-			return null;
+		log.info("Exiting Application");
+		System.exit(0);
+		return null;
 	}}
